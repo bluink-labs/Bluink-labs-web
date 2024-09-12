f637e0bea61a30d301dc92a8ad15b1ac29147dfe
ShanmugaRamana <shanmugaramana27@gmail.com>
2024-09-12 23:23:06 +0530

    Bluink

---
+++ script/newsletter-signup.js
@@ -0,0 +1,37 @@
+//routes
+document.getElementById("Home-footer").onclick = function(){
+    window.location.href = "index.html";
+};
+document.getElementById("products-footer").onclick = function(){
+    window.location.href = "products.html";
+};
+document.getElementById("media-footer").onclick = function(){
+    window.location.href = "media.html";
+};
+document.getElementById("developers-footer").onclick = function(){
+    window.open("https://bluink-labs.gitbook.io/bluink-labs-developer-docs","_blank");
+};
+document.getElementById("whitepaper-footer").onclick = function(){
+    window.open("/docs/whitepaper.pdf","_blank");
+};
+document.getElementById("contactus-footer").onclick = function(){
+    window.location.href = "contactus.html";
+};
+document.getElementById("signup").onclick = function(){
+    window.location.href = "newsletter-signup.html";
+};
+document.getElementById("linkedin").onclick = function() {
+    window.open("https://www.linkedin.com/company/bluink-labs", "_blank");
+};
+document.getElementById("github").onclick = function() {
+    window.open("", "_blank");
+};
+document.getElementById("x").onclick = function() {
+    window.open("https://x.com/bluink_labs", "_blank");
+};
+document.getElementById("instagram").onclick = function() {
+    window.open("https://www.instagram.com/bluink_labs/","_blank");
+};
+document.getElementById("developers-footer").onclick = function(){
+    window.open("https://bluink-labs.gitbook.io/bluink-labs-developer-docs","_blank");
+};
